3ff01d68eaff10dd28eef21406bd66a6d7acd50f
Ninelz <twilight9@outlook.com>
2018-05-25 21:09:25 +0800

    Test

--- src/Generator.java
+++ src/Generator.java
@@ -5,7 +5,7 @@ public class Generator {
         Account.register("ninel", "123456", false);
         Item.add("Nekopara", 10, "A galgame", 38);
         Item.add("Touhou 16", 10, "A STG", 60);
-        Item.add("Pillow test", 10, "Just a pillow again", 400);
+        Item.add("Pillow test again", 10, "Just a pillow again", 400);
         Account.save();
         Item.save();
     }
